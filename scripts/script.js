db1594c568256f6cdf5697355a1a3deace44ab8d
ahmedabdo404 <ahmed.elzahed404@gmail.com>
2022-02-14 22:33:57 +0200

    scroll button add and fixed some issues

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let upBtn = document.querySelector(".back-top")
+
+window.onscroll = () => {
+    // console.log(window.scrollY)
+    if(window.scrollY >= 650){
+        upBtn.style = ("opacity: 1;")
+    }else{
+        upBtn.style = ("opacity: 0;")
+    }
+}
+
+upBtn.onclick = () =>{
+    scrollTo({ top: 0, behavior: "smooth"});
+
+}
